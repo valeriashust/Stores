754fe07a64f21e4de1507c03649ed2fa5fdcf1ba
Valeria <valeriashust18@gmail.com>
2018-08-07 18:00:40 +0300

    initial commit

--- src/reducers/reducers.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from "redux";
-import stores from './stores';
-import error from './error';
-import success from './success';
-
-export default reducer = combineReducers({stores, error, success});
